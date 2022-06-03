106aad27f139e9c9d9b09f5bab04be8653a06359
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-06-03 16:02:52 +0530

    footer done

--- wp-content/themes/atlas/footer.php
+++ wp-content/themes/atlas/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,40 +12,37 @@
 
 ?>
 <div class="bottom-section" style="background-image: url('<?php echo get_template_directory_uri(); ?>/includes/img/bottom.jpg');">
-        <div class="footer-caption text-center">
-            <h4>විමසීම්</h4>
-            <h4 class="eng"><a target="_blank" href=""><i class="fa fa-phone"></i></a> <a target="_blank" href=""><i class="fa fa-whatsapp"></i></a> <a href="tel:<?php the_field('phone_number','option'); ?>"><?php the_field('phone_number','option'); ?></a></h4>
-        </div>
-
+    <div class="footer-caption text-center">
+        <h4>විමසීම්</h4>
+        <h4 class="eng"><a target="_blank" href="#"><i class="fa fa-phone"></i></a> <a target="_blank" href="#"><i class="fa fa-whatsapp"></i></a> <a href="tel:<?php the_field('phone_number', 'option'); ?>"><?php the_field('phone_number', 'option'); ?></a></h4>
     </div>
 
-    <!-- cards section end -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
-        crossorigin="anonymous"></script>
-    <!-- Owl Carousel -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <!-- Custom JS -->
-    <script src="<?php echo get_template_directory_uri(); ?>/includes/js/index.js"></script>
-    <script>
-        function myFunction(x) {
-            if (x.matches) { // If media query matches
-                document.getElementById('top-banner').style.backgroundImage = "url('<?php echo get_template_directory_uri(); ?>/includes/img/mobile-banner.jpg')";
-            } else {
-                document.getElementById('top-banner').style.backgroundImage = "url('<?php echo get_template_directory_uri(); ?>/includes/img/desktop-banner.jpg')";
-            }
-        }
+</div>
 
-        var x = window.matchMedia("(max-width: 576px)")
-        myFunction(x) // Call listener function at run time
-        x.addListener(myFunction) // Attach listener function on state changes
-    </script>
+<!-- cards section end -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+<!-- Owl Carousel -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<!-- Custom JS -->
+<script src="<?php echo get_template_directory_uri(); ?>/includes/js/index.js"></script>
+<script>
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            document.getElementById('top-banner').style.backgroundImage = "url('<?php echo get_template_directory_uri(); ?>/includes/img/mobile-banner.jpg')";
+        } else {
+            document.getElementById('top-banner').style.backgroundImage = "url('<?php echo get_template_directory_uri(); ?>/includes/img/desktop-banner.jpg')";
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 576px)")
+    myFunction(x) // Call listener function at run time
+    x.addListener(myFunction) // Attach listener function on state changes
+</script>
 
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
