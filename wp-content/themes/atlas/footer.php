610157a6ca873c8320813d356aea98b48793d0db
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-06-06 17:24:42 +0530

    before model

--- wp-content/themes/atlas/footer.php
+++ wp-content/themes/atlas/footer.php
@@ -22,6 +22,7 @@
 <!-- cards section end -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+
 <!-- Owl Carousel -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <!-- Custom JS -->
--- wp-content/themes/atlas/page-home.php
+++ wp-content/themes/atlas/page-home.php
@@ -2,39 +2,39 @@
 /*
 Template Name: Home
 */
- get_header();  ?>
+get_header();  ?>
 
 <!-- banner start -->
-<?php if( get_field('banner_image') ): ?>
-<div class="top-banner" id="top-banner" style="background-image: url('<?php the_field('banner_image'); ?>');">
-<?php endif; ?>        
-<div class="sign-btns">
-            <a class="login eng" href="<?php the_field('login_url');?>">login</a>
-            <a class="register eng" href="<?php the_field('register_url');?>">register</a>
-        </div>
-        <div class="banner-caption">
-            <div class="row">
-                <div class="col-sm-8 col-md-6">
-                    <div class="left-sec">
-                        <h2 class="sub-heading"><?php the_field('banner_title');?></h2>
-                        <h1 class="heading eng"><?php the_field('banner_eng_title');?></h1>
-                        <h2 class="sub-heading"><?php the_field('banner_sub_title');?></h2>
-                        <a class="reg-btn" href="<?php the_field('register_url');?>">ලියාපදිංචි වන්න</a>
-                    </div>
-                </div>
-                <div class="col-sm-4 col-md-6">
-                    <div class="right-sec">
-                        <h3 class="caption"><?php the_field('banner_caption_01');?> </h3>
-                        <h3 class="caption small-text"><?php the_field('banner_caption_02');?></h3>
-                    </div>
+<?php if (get_field('banner_image')) : ?>
+    <div class="top-banner" id="top-banner" style="background-image: url('<?php the_field('banner_image'); ?>');">
+    <?php endif; ?>
+    <div class="sign-btns">
+        <a class="login eng" href="<?php the_field('login_url'); ?>">login</a>
+        <a class="register eng" href="<?php the_field('register_url'); ?>">register</a>
+    </div>
+    <div class="banner-caption">
+        <div class="row">
+            <div class="col-sm-8 col-md-6">
+                <div class="left-sec">
+                    <h2 class="sub-heading"><?php the_field('banner_title'); ?></h2>
+                    <h1 class="heading eng"><?php the_field('banner_eng_title'); ?></h1>
+                    <h2 class="sub-heading"><?php the_field('banner_sub_title'); ?></h2>
+                    <a class="reg-btn" href="<?php the_field('register_url'); ?>">ලියාපදිංචි වන්න</a>
                 </div>
             </div>
-
+            <div class="col-sm-4 col-md-6">
+                <div class="right-sec">
+                    <h3 class="caption"><?php the_field('banner_caption_01'); ?> </h3>
+                    <h3 class="caption small-text"><?php the_field('banner_caption_02'); ?></h3>
+                </div>
+            </div>
         </div>
+
+    </div>
     </div>
     <div class="social">
-        <a class="s-fb" href="<?php the_field('');?>"><i class="fa fa-facebook"></i></a>
-        <a class="s-wp" href="<?php the_field('');?>"><i class="fa fa-whatsapp"></i></a>
+        <a class="s-fb" href="<?php the_field(''); ?>"><i class="fa fa-facebook"></i></a>
+        <a class="s-wp" href="<?php the_field(''); ?>"><i class="fa fa-whatsapp"></i></a>
     </div>
     <!-- banner end -->
     <!-- cards section start -->
@@ -55,16 +55,16 @@ Template Name: Home
     </div>
     <div class="third-sec" style="background-image: url('<?php echo get_template_directory_uri(); ?>/includes/img/3sec-web.jpg');">
         <div class="third-heading">
-            <h1 class="heading eng"><?php the_field('section_03_title');?></h1>
-            <h2 class="sub-heading"><?php the_field('section_03_sub_title');?></h2>
+            <h1 class="heading eng"><?php the_field('section_03_title'); ?></h1>
+            <h2 class="sub-heading"><?php the_field('section_03_sub_title'); ?></h2>
         </div>
         <div class="row">
             <div class="col-sm-6 col-md-4">
 
                 <div class="left-sec">
                     <div class="content">
-                        <h3> <i class="fa fa-book"></i> <?php the_field('sub_title_left');?></h3>
-                        <p><?php the_field('description_left');?> </p>
+                        <h3> <i class="fa fa-book"></i> <?php the_field('sub_title_left'); ?></h3>
+                        <p><?php the_field('description_left'); ?> </p>
                     </div>
                 </div>
             </div>
@@ -74,8 +74,8 @@ Template Name: Home
             <div class="col-sm-4 col-md-4">
                 <div class="right-sec">
                     <div class="content">
-                        <h3> <i class="fa fa-book"></i><?php the_field('sub_title_right');?></h3>
-                        <p><?php the_field('description_right');?></p>
+                        <h3> <i class="fa fa-book"></i><?php the_field('sub_title_right'); ?></h3>
+                        <p><?php the_field('description_right'); ?></p>
                     </div>
 
                 </div>
@@ -84,28 +84,27 @@ Template Name: Home
     </div>
     <div class="teacher-sec" style="background-image: url('<?php echo get_template_directory_uri(); ?>/includes/img/teacher-sec.jpg');">
         <div class="header text-center">
-            <h3><?php the_field('section_04_title');?></h3>
+            <h3><?php the_field('section_04_title'); ?></h3>
         </div>
 
         <div class="owl-carousel owl-theme">
-        <?php if( have_rows('teachers_slider') ): ?>
-                
-                <?php while( have_rows('teachers_slider') ): the_row(); 
+            <?php if (have_rows('teachers_slider')) : ?>
+
+                <?php while (have_rows('teachers_slider')) : the_row();
                     $image = get_sub_field('teacher_image');
-                    ?>
+                ?>
                     <div class="item text-center">
-                        <img src="<?php echo $image['url'];?>" alt="">
+                        <img src="<?php echo $image['url']; ?>" alt="">
                         <h3><?php the_sub_field('name_'); ?></h3>
                         <p><?php the_sub_field('description'); ?></p>
+
+
                     </div>
                 <?php endwhile; ?>
-                
+
             <?php endif; ?>
-            
-            
 
         </div>
     </div>
 
-
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
